dc81f1c369ba9d751f22a30049538262e07c4403
mmarcosa <marcos.barbosa8@fatec.sp.gov.br>
2020-10-28 02:08:59 -0300

    primeiro commit

---
+++ src/test/java/br/com/itau/chatbottelegram/chatbottelegram/ChatbottelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.chatbottelegram.chatbottelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbottelegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
